6680f70217207a83721f36c5f41f63d3254e1f91
Ryan Cheley <rcheley@gmail.com>
2022-08-31 19:45:57 -0700

    Added Suspension Feature Because Fernando Tatis Jr. took PEDs and got caught we have to account for cheaters. Update now has an ability to show if he's not committing errors because he's suspended for cheating

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 
-from content.models import InjuredList
+from content.models import InjuredList, SuspendedList
 
 
 @pytest.fixture
@@ -15,3 +15,17 @@ def on_injured_list():
     InjuredList.objects.create(is_injured=True)
     is_injured = InjuredList.objects.get(pk=1)
     return is_injured
+
+
+@pytest.fixture
+def not_suspended():
+    SuspendedList.objects.create(is_suspended=False)
+    is_suspended = SuspendedList.objects.get(pk=1)
+    return is_suspended
+
+
+@pytest.fixture
+def suspended():
+    SuspendedList.objects.create(is_suspended=True)
+    is_suspended = SuspendedList.objects.get(pk=1)
+    return is_suspended
